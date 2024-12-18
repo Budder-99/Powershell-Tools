6c7ee7e3b6838b817ad0e1f3494e71528dfaf9f4
Budder-99 <owenxbox99@gmail.com>
2024-12-18 08:58:51 +0000

    Uploaded Dark Moder
    Forces darkmode via powershell. Protect your eyes!

---
+++ DARK MODER.ps1
@@ -0,0 +1,30 @@
+﻿# Function to enable dark mode
+
+function Set-DarkMode {
+
+    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 0
+
+    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme" -Value 0
+
+    Write-Host -ForegroundColor Yellow "Dark mode enabled."
+
+}
+ 
+# Function to restart explorer to apply changes
+
+function Restart-Explorer {
+
+    Stop-Process -Name explorer -Force
+
+    Start-Sleep -Seconds 2
+
+    Start-Process explorer
+
+}
+ 
+# Enable dark mode and restart explorer
+
+Set-DarkMode
+
+Restart-Explorer
+ 
